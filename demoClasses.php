09f8b17b764e0de0ad7034e98c7b196f091f3bce
sammienjihia <dilaninjihia@gmail.com>
2019-05-24 11:50:26 +0300

    added comments

--- demoClasses.php
+++ demoClasses.php
@@ -2,13 +2,16 @@
 
 class printSth{
     
+    // Below variables demonstrates the class's properties
     var $first_name;
     var $last_name;
 
+    // The below function demonstrates the class's method
     function greetings(){
         echo "Hello ".$this->first_name." ".$this->last_name;
     }
 
+    // Below function demonstrates the class's constructor. This can allow an object to be instantiated with initial values
     function __construct($firstname, $lastname){
 
         $this->first_name = $firstname;
@@ -16,10 +19,10 @@ class printSth{
         
     }
 
-
     
 }
 
+// greetings is an object of the printSth class
 $greetings = new printSth("sammy", "njihia");
 $greetings->greetings();
 
